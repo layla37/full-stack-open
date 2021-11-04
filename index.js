16aec065876a946abad2e1edb4575b9d6a057cb0
Layla <laylamandella@gmail.com>
2021-11-04 16:28:52 -0700

    handle POST requests

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+}
+
+app.use(express.json());
+app.use(requestLogger);
+
 let notes = [
   {
     id: 1,
@@ -22,7 +33,14 @@ let notes = [
     date: "2019-05-30T19:20:14.298Z",
     important: true
   }
-]
+];
+
+const generateId = () => {
+  const maxId = notes.length > 0 
+  ? Math.max(...notes.map(n => n.id))
+  : 0;
+  return maxId + 1;
+};
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -43,11 +61,37 @@ app.get('/api/notes/:id', (request, response) => {
   }
 });
 
-app.delete('api/notes/:id', (req, res) => {
+app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id);
   notes = notes.filter(note => note.id !== id);
   response.status(204).end();
-})
+});
+
+app.post('/api/notes', (request, response) => {
+  const { body } = request;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  };
+
+  notes = notes.concat(note);
+  response.json(note);
+});
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({error: 'unknown endpoint'});
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001
 app.listen(PORT, () => {
